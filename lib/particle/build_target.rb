2d3f00248f3dad495fac6ce0c41ed9e71e3b7b97
Joe Goggins <joe.goggins@gmail.com>
2016-09-24 17:31:38 -0500

    Adds a VCR cassette and single BuildTarget test that is passing

--- lib/particle/build_target.rb
+++ lib/particle/build_target.rb
@@ -1,61 +1,26 @@
 require 'particle/model'
 module Particle
-
-  # Domain model for one Particle device
+  # Domain model for one Particle Build Target, underlying http api json looks like
+  #   {
+  #     "platforms": [
+  #       0,
+  #       8,
+  #       6,
+  #       10
+  #     ],
+  #     "prereleases": [
+  #       0,
+  #       8,
+  #       6,
+  #       10
+  #     ],
+  #     "firmware_vendor": "Particle",
+  #     "version": "0.6.0-rc.2"
+  #   },
   class BuildTarget < Model
     def initialize(client, attributes)
       super(client, attributes)
     end
-
-    # attribute_reader :url, :deviceID, :event, :created_at, :mydevices,
-    #   :requestType, :headers, :json, :query, :auth
-
-    # The response of the web server to a test message
-    # If nil, check error
-    # def response
-    #   get_attributes unless @loaded
-    #   @response
-    # end
-
-    # The error from the web server to a test message
-    # If nil, check response
-    # def error
-    #   get_attributes unless @loaded
-    #   @error
-    # end
-
-    # Force reloading the attributes for the webhook
-    # def get_attributes
-    #   @loaded = true
-    #   result = @client.webhook_attributes(self)
-    #   @response = result[:response]
-    #   @error = result[:error]
-    #   @attributes = result[:webhook]
-    # end
-    #
-    # Add a Particle webhook
-    # def create
-    #   new_webhook = @client.create_webhook(@attributes)
-    #   @attributes = new_webhook.attributes
-    #   self
-    # end
-    #
-    # Remove a Particle webhook
-    # def remove
-    #   @client.remove_webhook(self)
-    # end
-
-    # def self.list_path
-    #   "v1/webhooks"
-    # end
-    #
-    # def self.create_path
-    #   "v1/webhooks"
-    # end
-    #
-    # def path
-    #   "/v1/webhooks/#{id}"
-    # end
   end
 end
 
--- spec/particle/client/build_targets_spec.rb
+++ spec/particle/client/build_targets_spec.rb
@@ -2,4 +2,11 @@ require 'helper'
 require 'tempfile'
 
 describe Particle::Client::BuildTargets, :vcr do
+  describe ".build_targets" do
+    it "returns all available build targets" do
+      build_targets = Particle.build_targets
+      expect(build_targets).to be_kind_of Array
+      build_targets.each { |d| expect(d).to be_kind_of Particle::BuildTarget}
+    end
+  end
 end
