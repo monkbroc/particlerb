cfc2feef0ddcb4b042034a80089ac8a60570fb86
Joe Goggins <joe.goggins@gmail.com>
2016-09-24 17:31:38 -0500

    Adds a VCR cassette and single BuildTarget test that is passing

--- lib/particle/client/build_targets.rb
+++ lib/particle/client/build_targets.rb
@@ -4,7 +4,23 @@ require 'ostruct'
 module Particle
   class Client
     module BuildTargets
-      HTTP_PATH = "v1/binaries"
+      HTTP_PATH = "v1/build_targets"
+      # List all available Particle cloud compile build targets
+      #
+      # @return [Array<BuildTarget>] List of Particle Build Targets you can compile sources with
+      def build_targets
+        get(HTTP_PATH).map do |attributes|
+          build_target(attributes)
+        end
+      end
+
+      def build_target(attributes)
+        if attributes.is_a? BuildTarget
+          attributes
+        else
+          BuildTarget.new(self, attributes)
+        end
+      end
     end
   end
 end
